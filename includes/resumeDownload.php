b2d9d1c29c26bf9d478aa6d61eabb2503ce5e8ea
Dunya Kirkali <dunyakirkali@gmail.com>
2017-01-21 17:55:05 +0100

    Correct links to CV's

--- includes/resumeDownload.php
+++ includes/resumeDownload.php
@@ -1,7 +1,7 @@
 <div class="wrappedBox" id="foldBarWithLinks">
-    <a href="/files/CV-Lex-Postma-en-web.pdf" target="_blank" title="Download a pdf of my resumé in English" onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf English');" class="en">
+    <a href="/public-files/CV-Lex-Postma-en-web.pdf" target="_blank" title="Download a pdf of my resumé in English" onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf English');" class="en">
         <i class="fa fa-arrow-circle-down"></i>Resumé in English
-    </a><a href="/files/CV-Lex-Postma-nl-web.pdf" target="_blank" title="Download een pdf van mijn cv in het Nederlands" onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf Nederlands');" class="nl">
+    </a><a href="/files/public-CV-Lex-Postma-nl-web.pdf" target="_blank" title="Download een pdf van mijn cv in het Nederlands" onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf Nederlands');" class="nl">
         <i class="fa fa-arrow-circle-down"></i>CV in het Nederlands
     </a>
 </div>
