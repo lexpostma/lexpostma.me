7e07fb4979feef40049d67de030ffb40b9c15411
Lex Postma <lex@postma.me>
2017-09-02 16:57:02 +0200

    Test > dev

--- includes/domains.php
+++ includes/domains.php
@@ -2,20 +2,20 @@
     # DOMAINS
     $currentURL = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-    if (strpos($_SERVER['HTTP_HOST'], 'test.lexpostma.me') !== false) {
+    if (strpos($_SERVER['HTTP_HOST'], 'dev.lexpostma.me') !== false) {
         $test = true;
         
-        $portURL = "http://portfolio.test.lexpostma.me/";
-        $blogURL =      "http://blog.test.lexpostma.me/";
-        $resuURL =    "http://resume.test.lexpostma.me/";
-        $abouURL =           "http://test.lexpostma.me/";
+        $portURL = "http://portfolio.dev.lexpostma.me/";
+        $blogURL =      "http://blog.dev.lexpostma.me/";
+        $resuURL =    "http://resume.dev.lexpostma.me/";
+        $abouURL =           "http://dev.lexpostma.me/";
         
     	// MAMP testing URLs
-    	    if($_SERVER['HTTP_HOST'] ==      'blog.test.lexpostma.me'){ $basepage = "blog";      $baseURL = $blogURL; }
-    	elseif($_SERVER['HTTP_HOST'] ==    'resume.test.lexpostma.me'){ $basepage = "resume";    $baseURL = $resuURL; }
-        elseif($_SERVER['HTTP_HOST'] == 'portfolio.test.lexpostma.me'){ $basepage = "portfolio"; $baseURL = $portURL; }
-    	elseif($_SERVER['HTTP_HOST'] ==     'about.test.lexpostma.me'){ $basepage = "about";     $baseURL = $abouURL; } // legacy URL
-    	elseif($_SERVER['HTTP_HOST'] ==           'test.lexpostma.me'){ $basepage = "about";     $baseURL = $abouURL; };
+    	    if($_SERVER['HTTP_HOST'] ==      'blog.dev.lexpostma.me'){ $basepage = "blog";      $baseURL = $blogURL; }
+    	elseif($_SERVER['HTTP_HOST'] ==    'resume.dev.lexpostma.me'){ $basepage = "resume";    $baseURL = $resuURL; }
+        elseif($_SERVER['HTTP_HOST'] == 'portfolio.dev.lexpostma.me'){ $basepage = "portfolio"; $baseURL = $portURL; }
+    	elseif($_SERVER['HTTP_HOST'] ==     'about.dev.lexpostma.me'){ $basepage = "about";     $baseURL = $abouURL; } // legacy URL
+    	elseif($_SERVER['HTTP_HOST'] ==           'dev.lexpostma.me'){ $basepage = "about";     $baseURL = $abouURL; };
 
         $coreURL = $portURL;
     }
