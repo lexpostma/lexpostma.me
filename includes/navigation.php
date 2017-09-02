c9781c981de2acc1cfe3a88cb371d9dd83eb59dd
Lex Postma <lex@postma.me>
2017-09-02 15:37:37 +0200

    Refactoring $navName again

--- includes/navigation.php
+++ includes/navigation.php
@@ -9,9 +9,9 @@
             </a>
         </header>
         <nav id="mainNavigation">
-            <a href="/"             title="Go back"                                                                        ><? include 'navigationIcons/back.svg'     ?></a>
-            <a href="<?=$portURL?>" title="Portfolio" class="<? if( $navigationName == 'portfolio'){ echo('active'); } ?>" ><? include 'navigationIcons/portfolio.svg'?><span>Portfolio</span></a>
-            <a href="<?=$blogURL?>" title="Blog"      class="<? if( $navigationName == 'blog'){ echo('active'); }      ?>" ><? include 'navigationIcons/blog.svg'     ?><span>Blog</span></a>
-            <a href="<?=$resuURL?>" title="Resume"    class="<? if( $navigationName == 'resume'){ echo('active'); }    ?>" ><? include 'navigationIcons/resume.svg'   ?><span>Resumé</span></a>
-            <a href="<?=$abouURL?>" title="About me"  class="<? if( $navigationName == 'about'){ echo('active'); }     ?>" ><? include 'navigationIcons/about.svg'    ?><span>About me</span></a>
+            <a href="<?=$baseURL?>" title="Back to <? echo $pageTitle ?>"                                            ><? include 'navigationIcons/back.svg'     ?></a>
+            <a href="<?=$portURL?>" title="Portfolio" class="<? if( $homepage == 'portfolio'){ echo('active'); } ?>" ><? include 'navigationIcons/portfolio.svg'?><span>Portfolio</span></a>
+            <a href="<?=$blogURL?>" title="Blog"      class="<? if( $homepage == 'blog'){ echo('active'); }      ?>" ><? include 'navigationIcons/blog.svg'     ?><span>Blog</span></a>
+            <a href="<?=$resuURL?>" title="Resume"    class="<? if( $homepage == 'resume'){ echo('active'); }    ?>" ><? include 'navigationIcons/resume.svg'   ?><span>Resumé</span></a>
+            <a href="<?=$abouURL?>" title="About me"  class="<? if( $homepage == 'about'){ echo('active'); }     ?>" ><? include 'navigationIcons/about.svg'    ?><span>About me</span></a>
         </nav>
