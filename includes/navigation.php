f9d66762323f007b992a788b222982e144cd2b41
Lex Postma <lex@postma.me>
2017-09-02 14:59:06 +0200

    Creating the tab bar navigation

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,11 +1,11 @@
-        <header id="title-header">
+        <header id="titleHeader">
             <? echo $pageTitle ?>
             Logo
         </header>
-        <nav>
-            <a href="/"             title="Go back"   >Back</a>
-            <a href="<?=$portURL?>" title="Portfolio" >Portfolio</a>
-            <a href="<?=$blogURL?>" title="Blog"      >Blog</a>
-            <a href="<?=$resuURL?>" title="Resume"    >Resumé</a>
-            <a href="<?=$abouURL?>" title="About me"  >About me</a>
+        <nav id="mainNavigation">
+            <a href="/"             title="Go back"                                                                        ><? include 'navigationIcons/back.svg'     ?></a>
+            <a href="<?=$portURL?>" title="Portfolio" class="<? if( $navigationName == 'portfolio'){ echo('active'); } ?>" ><? include 'navigationIcons/portfolio.svg'?><span>Portfolio</span></a>
+            <a href="<?=$blogURL?>" title="Blog"      class="<? if( $navigationName == 'blog'){ echo('active'); }      ?>" ><? include 'navigationIcons/blog.svg'     ?><span>Blog</span></a>
+            <a href="<?=$resuURL?>" title="Resume"    class="<? if( $navigationName == 'resume'){ echo('active'); }    ?>" ><? include 'navigationIcons/resume.svg'   ?><span>Resumé</span></a>
+            <a href="<?=$abouURL?>" title="About me"  class="<? if( $navigationName == 'about'){ echo('active'); }     ?>" ><? include 'navigationIcons/about.svg'    ?><span>About me</span></a>
         </nav>
