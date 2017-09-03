28e4fb886cab2c455de8aaab4b2ccc8ed7f60325
Lex Postma <lex@postma.me>
2017-09-03 22:15:57 +0200

    Applying iOS-style to resume downloads

--- includes/resumeDownload.php
+++ includes/resumeDownload.php
@@ -1,7 +1,17 @@
-<div class="wrappedBox" id="foldBarWithLinks">
-    <a href="/public-files/CV-Lex-Postma-en-web.pdf" target="_blank" title="Download a pdf of my resumé in English" onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf English');" class="en">
-        <i class="fa fa-arrow-circle-down"></i>Resumé in English
-    </a><a href="/public-files/CV-Lex-Postma-nl-web.pdf" target="_blank" title="Download een pdf van mijn cv in het Nederlands" onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf Nederlands');" class="nl">
-        <i class="fa fa-arrow-circle-down"></i>CV in het Nederlands
+<div class="cellRowGroup">
+    <h6>Download a pdf of my resume</h6>
+    <a class="cellRow en" href="/public-files/CV-Lex-Postma-en-web.pdf" 
+            target="_blank" title="Download a pdf of my resumé in English" 
+            onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf English');">
+        <div class="cellIcon" id="flagNL"></div>
+        <span class="cellLabel">Resumé in English<!-- <i class="fa fa-arrow-down"></i> --></span>
+        <div class="cellClosingIcon download"><? include 'navigationIcons/download.svg'  ?></div>
+    </a>
+    <a  class="cellRow nl" href="/public-files/CV-Lex-Postma-nl-web.pdf" 
+            target="_blank" title="Download een pdf van mijn cv in het Nederlands" 
+                onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf Nederlands');">
+        <div class="cellIcon" id="flagEN"></div>
+        <span class="cellLabel">CV in het Nederlands<!-- <i class="fa fa-arrow-down"></i> --></span>
+        <div class="cellClosingIcon download"><? include 'navigationIcons/download.svg'  ?></div>
     </a>
 </div>
