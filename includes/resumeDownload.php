ac71d15fd134e5536b1125eb6f8bdfbe221ff5d8
Lex Postma <lex@postma.me>
2017-09-06 17:27:31 +0200

    Adding flag icons to resume download buttons

--- includes/resumeDownload.php
+++ includes/resumeDownload.php
@@ -1,18 +1,18 @@
 <ul class="cellRowGroup">
-    <li class="cellRow">
-        <a class="en" href="/public-files/CV-Lex-Postma-en-web.pdf" 
+    <li class="cellRow resumeDownloadItem">
+        <a href="/public-files/CV-Lex-Postma-en-web.pdf" 
                 target="_blank" title="Download a pdf of my resumé in English" 
                 onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf English');">
-            <div class="cellIcon" id="flagNL"></div>
+            <div class="cellIcon"><? include 'navigationIcons/flagGB.svg'  ?></div>
             <span class="cellLabel">Resumé in English</span>
             <div class="cellClosingIcon download"><? include 'navigationIcons/download.svg'  ?></div>
         </a>
     </li>
-    <li class="cellRow">
-        <a  class="nl" href="/public-files/CV-Lex-Postma-nl-web.pdf" 
+    <li class="cellRow resumeDownloadItem">
+        <a href="/public-files/CV-Lex-Postma-nl-web.pdf" 
                 target="_blank" title="Download een pdf van mijn cv in het Nederlands" 
                     onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf Nederlands');">
-            <div class="cellIcon" id="flagEN"></div>
+            <div class="cellIcon"><? include 'navigationIcons/flagNL.svg'  ?></div>
             <span class="cellLabel">CV in het Nederlands</span>
             <div class="cellClosingIcon download"><? include 'navigationIcons/download.svg'  ?></div>
         </a>
