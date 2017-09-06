045c19abd0d49e264ff9e50bb1cc305a8e6a9ad3
Lex Postma <lex@postma.me>
2017-09-06 16:52:25 +0200

    Removed the contact block on the about page

--- includes/about.php
+++ includes/about.php
@@ -5,7 +5,7 @@
 
 <div id="aboutColumns">
     <article class="contentBlock"><?          echo mysqli_fetch_array(mysqli_query($con, "SELECT body FROM about WHERE name = 'me';"))[0]; ?></article>
-    <div class="contentBlock columnBreak"><?  include '../includes/aboutContact.php'; ?></div>
+<!-- //     <div class="contentBlock columnBreak"><?  include '../includes/aboutContact.php'; ?></div> -->
     <article class="contentBlock"><?          include '../includes/aboutDetails.php'; ?></article>
     <article class="contentBlock"><?          echo mysqli_fetch_array(mysqli_query($con, "SELECT body FROM about WHERE name = 'internetthing';"))[0]; ?></article>
 
