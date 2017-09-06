6596f709448cd4b0858405a6cef9882c736a487d
Lex Postma <lex@postma.me>
2017-09-06 17:26:25 +0200

    Adding and styling contact items to the drawer

--- includes/aboutContact.php
+++ includes/aboutContact.php
@@ -1,3 +1,5 @@
+<ul class="cellRowGroup">
+
 <?
     $contactQuery = "SELECT * FROM about_contact WHERE published = '1' ";
     if( !$detect ->isMobile()) {
@@ -5,9 +7,6 @@
     }
     $contactQuery .= "ORDER BY volgorde ASC;";
     $contactResult = mysqli_query($con,$contactQuery);
-
-    $contactTexts = '<div class="contact-text"><p id="s-contact">You should be able to find me somewhere...</p>';
-    $contactIcons = '<div class="contact-buttons">';
     
 	while($row = mysqli_fetch_array($contactResult)){
 		$network    = $row['network'];
@@ -16,20 +15,24 @@
 		$webtext    = $row['webtext'];
 		$brandcolor = $row['brandcolor'];
         $svgCode    = $row['svgCode'];
-        $style      = "fill: #$brandcolor;";
 
-        $mouseover = "show('s-$shortname'); hide('s-contact');"; 
-        $mouseout  = "hide('s-$shortname'); show('s-contact');";
         $gaEvent   = "ga('send', 'event', 'About', 'Contact', '$network');";
-
-        $contactTexts .= "<p id='s-$shortname' style='display:none'>$webtext</p>";
-        $contactIcons .= '<a href="'.$link.'" title="'.$network.'" class="button b-'.$shortname.'" onMouseOver="'.$mouseover.'" onMouseOut="'.$mouseout.'" onclick="'.$gaEvent.'" ><svg brandcolor="#'.$brandcolor.'" style="'.$style.'" width="48px" height="48px" viewBox="0 0 100 100" fill-rule="evenodd">
-                <g>
-                    '.$svgCode.'
-                </g>
-            </svg></a>';
-	}
-
-	echo $contactTexts .= '<p style="display:none"></p></div>';
-	echo $contactIcons .= '</div>';
 ?>
+
+    <li class="cellRow contactItem">
+        <a href="<?=$link?>" 
+                target="_blank" title="<?=$network?>" 
+                onclick="<?=$gaEvent?>">
+            <div class="cellIcon <?=$shortname?>" id="contactIcon_<?=$shortname?>" style="background-color: #<?=$brandcolor?>;">
+                <svg xmlns="http://www.w3.org/2000/svg" width="29" height="29" viewBox="0 0 100 100" style="fill: white;" ><?=$svgCode?></svg>
+            </div>
+            <span class="cellLabel"><?=$network?></span>
+            <div class="cellClosingIcon chevron"><? include 'navigationIcons/chevron.svg'  ?></div>
+        </a>
+    </li>
+
+<?
+	}
+?>
+
+</ul>
