ca755e52465f2c8a273db64692f6ef73279a7c8a
Lex Postma <lex@postma.me>
2017-09-07 16:08:38 +0200

    Added GA click events, improved code readability

--- includes/navigationTabbar.php
+++ includes/navigationTabbar.php
@@ -3,11 +3,12 @@
 <?
 // Title bar and secundary navigation are in index.php
 
+
 # ===================================== #
 # ==== Primary navigation, tab bar ==== #
 # ===================================== #
 
-
+    $gaMainNavEvent = " 'send', 'event', 'Navigation', 'Navigate tab bar',";
 ?>
             <nav id="tabbarNavigation">
                 <ul id="tabbarItemList">
@@ -16,7 +17,7 @@
     if( $basepageTwo !== 'home' && $basepageTwo !== 'filtered' ){
 ?>
                     <li class="tabbarItem" id="tabbarSpecialItem">
-                        <a href="<?=$baseURL?>" title="Back to <? echo $pageTitle ?>" class="tabbarLink noLabel"><? include 'navigationIcons/back.svg'  ?></a>
+                        <a href="<?=$baseURL?>" title="Back to <?=$basepageTitle ?>" class="tabbarLink noLabel" onclick="ga(<?=$gaMainNavEvent?> 'Back to <?=$basepage?>');"><? include 'navigationIcons/back.svg'  ?></a>
                     </li>
 <?        
     } else {
@@ -27,7 +28,7 @@
                     <li class="tabbarItem" id="tabbarSpecialItem">
                         <a class="tabbarLink" href="#" 
                                 title="<?=$tabbarSpecialItemTitle?>" 
-                                onclick="toggleDrawer(); changeItem('tabbarTooltip')">
+                                onclick="toggleDrawer(); ga(<?=$gaMainNavEvent?> 'Toggle drawer on <?=$basepage?>'); changeItem('tabbarTooltip')">
 <?
         if($tabbarSpecialItemTooltip){
 ?>
@@ -51,25 +52,37 @@
 
 ?>
                     <li class="tabbarItem">
-                        <a href="<?=$portURL?>" title="Portfolio" class="tabbarLink <? if( $basepage == 'portfolio'){ echo('active'); } ?>" >
+                        <a class="tabbarLink <? if( $basepage == 'portfolio'){ echo('active'); } ?>" 
+                                href="<?=$portURL?>" 
+                                onclick="ga(<?=$gaMainNavEvent?> 'Portfolio');"
+                                title="Portfolio" >
                             <? include 'navigationIcons/portfolio.svg'?>
                             <span class="tabName">Portfolio</span>
                         </a>
                     </li>
                     <li class="tabbarItem">
-                        <a href="<?=$blogURL?>" title="Blog"      class="tabbarLink <? if( $basepage == 'blog'){      echo('active'); } ?>" >
+                        <a class="tabbarLink <? if( $basepage == 'blog'){      echo('active'); } ?>"
+                                href="<?=$blogURL?>" 
+                                onclick="ga(<?=$gaMainNavEvent?> 'Blog');"
+                                title="Blog" >
                             <? include 'navigationIcons/blog.svg'     ?>
                             <span class="tabName">Blog</span>
                         </a>
                     </li>
                     <li class="tabbarItem">
-                        <a href="<?=$resuURL?>" title="Resume"    class="tabbarLink <? if( $basepage == 'resume'){    echo('active'); } ?>" >
+                        <a class="tabbarLink <? if( $basepage == 'resume'){    echo('active'); } ?>" 
+                                href="<?=$resuURL?>"
+                                onclick="ga(<?=$gaMainNavEvent?> 'Resumé');"
+                                title="Resumé" >
                             <? include 'navigationIcons/resume.svg'   ?>
                             <span class="tabName">Resumé</span>
                         </a>
                     </li>
                     <li class="tabbarItem">
-                        <a href="<?=$abouURL?>" title="About Lex" class="tabbarLink <? if( $basepage == 'about'){     echo('active'); } ?>" >
+                        <a class="tabbarLink <? if( $basepage == 'about'){     echo('active'); } ?>"
+                                href="<?=$abouURL?>" 
+                                onclick="ga(<?=$gaMainNavEvent?> 'About');"
+                                title="About Lex" >
                             <? include 'navigationIcons/about.svg'    ?>
                             <span class="tabName">About Lex</span>
                         </a>
--- includes/navigationTitle.php
+++ includes/navigationTitle.php
@@ -1,3 +1,6 @@
+<?
+    $gaSecondaryNavEvent = " 'send', 'event', 'Navigation', 'Navigate title bar',";
+?>
             <header id="titleHeader">
                 <span id="mainTitle"><a href="<?=$baseURL ?>"><?=$basepageTitle ?></a></span>
                 <a id="mainLogo" href="/">
@@ -9,13 +12,33 @@
 <?
     if( $basepage == 'blog' ){
 ?>
-                    <li><a href="#">Subscribe</a></li>
-                    <li><a href="<? echo $blogTwitterURL ?>"><i class="fa fa-twitter"></i></a></li>
+                    <li>
+                        <a href="<?=$blogFeedURL?>" 
+                                title="Subscribe to the RSS feed" 
+                                onclick="ga(<?=$gaSecondaryNavEvent?> 'Subscribe to RSS');" >Subscribe</a>
+                    </li>
+                    <li>
+                        <a href="<?=$blogTwitterURL ?>"
+                                title="Follow @lexpostmame on Twitter" 
+                                onclick="ga(<?=$gaSecondaryNavEvent?> 'Follow @lexpostmame');" >
+                            <i class="fa fa-twitter"></i>
+                        </a>
+                    </li>
 <?
     } elseif ( $basepage == 'resume' ){
 ?>
-                    <li><a href="#references">References</a></li>
-                    <li><a href="<? echo $linkedinURL ?>"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li>
+                        <a href="#references"
+                                title="References, what people are saying about me" 
+                                onclick="ga(<?=$gaSecondaryNavEvent?> 'References');" >References</a>
+                    </li>
+                    <li>
+                        <a href="<?=$linkedinURL ?>"
+                                title="Connect with me on LinkedIn" 
+                                onclick="ga(<?=$gaSecondaryNavEvent?> 'LinkedIn');" >
+                            <i class="fa fa-linkedin-square"></i>
+                        </a>
+                    </li>
 <?
     } 
 ?>
