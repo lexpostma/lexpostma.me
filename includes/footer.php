8212d8c67a690a64a40305d0b6849faf3f33d50c
Lex Postma <lex@postma.me>
2017-09-07 16:08:54 +0200

    Added <p> to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,7 @@
 <footer id="main-footer" class="copyright">
-    <span>Designed and built by Lex Postma</span>
-    <span>&copy; 2010 - <?=date('Y')?>, though <a href="<?=$coreURL?>credits" title="Credits" onclick="ga('send', 'event', 'Navigation', 'Footer', 'Credits');">credits</a> where credit’s due.</span>
-    <span>Enjoy the rest of your <?=date('l')?>!</span>
+    <p>
+        <span>Designed and built by Lex Postma</span>
+        <span>&copy; 2010 - <?=date('Y')?>, though <a href="<?=$coreURL?>credits" title="Credits" onclick="ga('send', 'event', 'Navigation', 'Footer', 'Credits');">credits</a> where credit’s due.</span>
+        <span>Enjoy the rest of your <?=date('l')?>!</span>
+    </p>
 </footer>
