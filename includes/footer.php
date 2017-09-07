b0b90a473507d36cd497356eb30e377ed1618b06
Lex Postma <lex@postma.me>
2017-09-07 17:18:05 +0200

    Adjusted the footer text and elements

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,6 @@
-<footer id="main-footer" class="copyright">
-    <p>
-        <span>Designed and built by Lex Postma</span>
-        <span>&copy; 2010 - <?=date('Y')?>, though <a href="<?=$coreURL?>credits" title="Credits" onclick="ga('send', 'event', 'Navigation', 'Footer', 'Credits');">credits</a> where credit’s due.</span>
-        <span>Enjoy the rest of your <?=date('l')?>!</span>
+<footer id="pageFooter" class="copyright">
+    <p>Designed and built by Lex Postma <span class="nowrap">&copy; 2010 - <?=date('Y')?></span>
+    <br><a href="<?=$coreURL?>credits" title="Credits" onclick="ga('send', 'event', 'Navigation', 'Footer', 'Credits');">Credits</a> where credit’s due.
+    <br>Enjoy the rest of your <?=date('l')?>!
     </p>
 </footer>
