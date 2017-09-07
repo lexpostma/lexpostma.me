2460d9348a25453a94af36c80e98310153f064da
Lex Postma <lex@postma.me>
2017-09-07 17:20:07 +0200

    Moved a lot of CSS code from old global to new global

--- includes/head.php
+++ includes/head.php
@@ -83,7 +83,8 @@
 
         <!-- Styles and scripts -->
         <link rel="stylesheet" href="/styles/normalize.css">
-        <link rel="stylesheet" href="/styles/global.css">
+
+<!--         <link rel="stylesheet" href="/styles/global.css"> -->
         <link rel="stylesheet" href="/styles/global-new.css">
         <link rel="stylesheet" href="/styles/navigation.css">
         <link rel="stylesheet" href="/styles/<?=$basepage?>.css">
