311eac39436a846f6a43f1aa1a52df1c74dbf72a
Lex Postma <lex@postma.me>
2017-09-10 21:32:38 +0200

    Improved footer “Enjoy rest of your day!” text
    Made the day client side with JS, instead of serverside with PHP

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,21 @@
 <footer id="pageFooter" class="copyright">
     <p>Designed and built by Lex Postma <span class="nowrap">&copy; 2010 - <?=date('Y')?></span>
-    <br><a href="<?=$coreURL?>credits" title="Credits" onclick="ga('send', 'event', 'Navigation', 'Footer', 'Credits');">Credits</a> where credit’s due.
-    <br>Enjoy the rest of your <?=date('l')?>!
+        <br><a href="<?=$coreURL?>credits" title="Credits" onclick="ga('send', 'event', 'Navigation', 'Footer', 'Credits');">Credits</a> where credit’s due.
+        <br>Enjoy the rest of your 
+        <script>
+            var day = new Date();
+            var weekday = new Array(7);
+            weekday[0] = "Sun";
+            weekday[1] = "Mon";
+            weekday[2] = "Tues";
+            weekday[3] = "Wednes";
+            weekday[4] = "Thurs";
+            weekday[5] = "Fri";
+            weekday[6] = "Satur";
+            
+            var n = weekday[day.getDay()];
+        
+            document.write(n);
+        </script>day!
     </p>
 </footer>
