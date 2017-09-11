0bb5a7db9bd5b04c16eef4d8f5b12ff17d8e2455
Lex Postma <lex@postma.me>
2017-09-11 20:14:13 +0200

    Remove search indexing of test/dev environments

--- includes/head.php
+++ includes/head.php
@@ -6,7 +6,7 @@
         <title><?=$seoTitle?></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
 <?
-    if($basepageTwo == 'apple'){?>
+    if($basepageTwo == 'apple' || $currentEnvironment !== 'production'){?>
         <meta name="robots" content="noindex" />
 <?
     };
