715cb927f46ecf4be4d108e043d238946dac8b9d
Lex Postma <lex@postma.me>
2017-09-12 17:39:42 +0200

    Completely revisit filtering

--- includes/portfolioFilters.php
+++ includes/portfolioFilters.php
@@ -4,19 +4,15 @@
 # ==== Client filter ==== #
 # ======================= #
 
-	if(empty($clientFilter)){
-        $selectClient = "<option>Select</option><option disabled></option>"; 
-	} else { 
-    	$selectClient = "";
-    };
+    $selectClient = "<option value=''>Select</option><option disabled></option>"; 
 
     if(isset($clientFilter) && $clientFilter == 'tudelft-master'){
-                                                        $selectClient .= "<option value='".makeNewFilterURL('client')."&client=".$shortclient2."' selected >TU Delft Master</option>
-                                                        <option value='".makeNewFilterURL('client')."&client=tudelft-bachelor' >From TU Delft Bachelor</option>
+                                                        $selectClient .= "<option value='".$shortclient2."' selected >TU Delft Master</option>
+                                                        <option value='tudelft-bachelor' >TU Delft Bachelor</option>
                                                         <option disabled></option>";
     } elseif(isset($clientFilter) && $clientFilter == 'tudelft-bachelor'){
-                                                        $selectClient .= "<option value='".makeNewFilterURL('client')."&client=".$shortclient2."' selected >TU Delft Bachelor</option>
-                                                        <option value='".makeNewFilterURL('client')."&client=tudelft-master' >From TU Delft Master</option>
+                                                        $selectClient .= "<option value='".$shortclient2."' selected >TU Delft Bachelor</option>
+                                                        <option value='tudelft-master' >TU Delft Master</option>
                                                         <option disabled></option>";            
     };
 
@@ -36,7 +32,7 @@
         $client2 = $row['client'];
         if(strpos($shortclient2,'tudelft') !== false){ $shortclient2 = 'tudelft'; }; // remove the -bachelor/-master from tudelft
 
-        $selectClient .= "<option value='".makeNewFilterURL('client')."&client=".$shortclient2."' ";
+        $selectClient .= "<option value='".$shortclient2."' ";
         if(isset($clientFilter) && $clientFilter == $shortclient2){   $selectClient .= "selected";    };
         $selectClient .= ">".$client2."</option>";
 
@@ -47,8 +43,7 @@
 # ==== Category filter ==== #
 # ========================= #
 
-	if(empty($categoryFilter)){ $selectCategory = "<option>Select</option><option disabled></option>"; }
-	else{                       $selectCategory = ""; };
+    $selectCategory = "<option value=''>Select</option><option disabled></option>";
     
     $categoryFilterSelectionQuery = "SELECT category,shortcategory 
                                      FROM portfolio 
@@ -61,7 +56,7 @@
     
     while($row = mysqli_fetch_array($categoryFilterSelection)){
 
-        $selectCategory .= "<option value='".makeNewFilterURL('category')."&category=".$row['shortcategory']."' ";
+        $selectCategory .= "<option value='".$row['shortcategory']."' ";
         if(isset($categoryFilter) && $categoryFilter == $row['shortcategory']){  $selectCategory .= "selected";  };
         $selectCategory .= ">".$row['category']."</option>";
 
@@ -71,8 +66,7 @@
 # ==== Year filter ==== #
 # ===================== #
 
-	if(empty($yearFilter)){   $selectYear = "<option>Select</option><option disabled></option>"; }
-	else{                     $selectYear = ""; };
+    $selectYear = "<option value=''>Select</option><option disabled></option>";
 
     $yearFilterSelectionQuery = "SELECT year 
                                  FROM portfolio 
@@ -84,7 +78,7 @@
     
     while($row = mysqli_fetch_array($yearFilterSelection)){
         
-        $selectYear .= "<option value='".makeNewFilterURL('year')."&year=".$row['year']."' ";
+        $selectYear .= "<option value='".$row['year']."' ";
         if(isset($yearFilter) && $yearFilter == $row['year']){  $selectYear .= "selected"; };
         $selectYear .= ">".$row['year']."</option>";
         
@@ -95,97 +89,83 @@
 # ====================== #
 
     if(isset($typeFilter) && $typeFilter == 'video'){ 
-        $videoCheckbox = ' checked value="'.makeNewFilterURL('type').'" ';
+        $videoCheckbox = ' checked value="video" ';
     } else {
-        $videoCheckbox = 'value="'.makeNewFilterURL('type').'&type=video" ';
+        $videoCheckbox = 'value="video" ';
     };
 
 
 ?>
 
+
 <ul class="cellRowGroup">
-    <form action="/search.php" method="get">
+    <form action="/filtering.php" method="get">
         <li class="cellRow <?if(isset($searchFilter)){ echo'filterOn';}?>">
-            <div class="cellRowContent" onclick="focusOnInput('filterInputSearch');">
+            <div class="cellRowContent">
                 <div class="cellIcon search"><i class="fa fa-fw fa-search"></i></div>
                 <label for="inputSearch" class="cellLabel">Search</label>
-                <div class="cellValue">
-                    <input type="hidden" name="rest"   value="<?=makeNewFilterURL('search');?>">
-                    <input type="search" name="search" placeholder="Type" title="Search for projects" id="inputSearch"
-                        <? if(isset($searchFilter)){ echo ' value="'.$searchFilter.'" ';}?>>
-                </div>
-                <a class="cellClosingIcon deleteFilter" href="<?=makeNewFilterURL('search')?>">&times;</a>
+                <input class="cellValue" title="Search for projects" type="search" name="search" id="inputSearch" placeholder="Type" <? if(isset($searchFilter)){ echo ' value="'.$searchFilter.'" ';}?>>
+                <button type="button" class="cellClosingIcon deleteFilter" onclick="clearInput('inputSearch')">&times;</button>
             </div>
         </li>
-        <li class="cellRow <?if(isset($categoryFilter)){ echo'filterOn';}?>">
-            <div class="cellRowContent" onclick="focusOnInput('filterSelectCategory');">
+
+
+        
+        <li class="cellRow <?if(isset($categoryFilter)){ echo'filterOn';}?>">        
+            <div class="cellRowContent">
                 <div class="cellIcon tag"><i class="fa fa-fw fa-tag"></i></div>
                 <label for="inputCategory" class="cellLabel">Category</label>
-                <div class="cellValue">
-                    <select onchange="window.open(this.value,'_self');" title="Filter by category" id="inputCategory">
-                        <?=$selectCategory?>
-                    </select>
-                </div>
-                <a class="cellClosingIcon deleteFilter" href="<?=makeNewFilterURL('category')?>">&times;</a>
+                <select class="cellValue" title="Filter by category" name= "category" id="inputCategory"><?=$selectCategory?></select>
+                <span id="inputCategoryWidth" class="widthForSelectHidden"></span>
+                <button type="button" class="cellClosingIcon deleteFilter" onclick="clearInput('inputCategory')">&times;</button>
             </div>
         </li>
-        <li class="cellRow <?if(isset($yearFilter)){ echo'filterOn';}?>">
-            <div class="cellRowContent" onclick="focusOnInput('filterSelectYear');">
+
+
+
+        <li class="cellRow <?if(isset($yearFilter)){ echo'filterOn';}?>">        
+            <div class="cellRowContent">
                 <div class="cellIcon date"><i class="fa fa-fw fa-calendar"></i></div>
                 <label for="inputYear" class="cellLabel">Year</label>
-                <div class="cellValue">
-                    <select onchange="window.open(this.value,'_self');" title="Filter by year" id="inputYear">
-                        <?=$selectYear?>
-                    </select>
-                </div>
-                <a class="cellClosingIcon deleteFilter" href="<?=makeNewFilterURL('year')?>">&times;</a>
+                <select class="cellValue" title="Filter by year" name= "year" id="inputYear"><?=$selectYear?></select>
+                <span id="inputYearWidth" class="widthForSelectHidden"></span>
+                <button type="button" class="cellClosingIcon deleteFilter" onclick="clearInput('inputYear')">&times;</button>
             </div>
         </li>
-        <li class="cellRow <?if(isset($clientFilter)){ echo'filterOn';}?>">
-            <div class="cellRowContent" onclick="focusOnInput('filterSelectClient');">
+        
+
+
+        <li class="cellRow <?if(isset($clientFilter)){ echo'filterOn';}?>">        
+            <div class="cellRowContent">
                 <div class="cellIcon client"><i class="fa fa-fw fa-building"></i></div>
                 <label for="inputClient" class="cellLabel">Client</label>
-                <div class="cellValue">
-                    <select onchange="window.open(this.value,'_self');" title="Filter by client" id="inputClient">
-                        <?=$selectClient?>
-                    </select>
-                </div>
-                <a class="cellClosingIcon deleteFilter" href="<?=makeNewFilterURL('client')?>">&times;</a>
+                <select class="cellValue" title="Filter by client" name= "client" id="inputClient"><?=$selectClient?></select>
+                <span id="inputClientWidth" class="widthForSelectHidden"></span>
+                <button type="button" class="cellClosingIcon deleteFilter" onclick="clearInput('inputClient')">&times;</button>
             </div>
         </li>
-        <li class="cellRow <?if(isset($typeFilter) && $typeFilter == 'video'){ echo'filterOn';}?>">
-            <div class="cellRowContent" onclick="focusOnInput('toggleVideosOnly');">
+        
+
+
+        <li class="cellRow <?if(isset($typeFilter) && $typeFilter == 'video'){ echo'filterOn';}?>">        
+            <div class="cellRowContent">
                 <div class="cellIcon video"><i class="fa fa-fw fa-video-camera"></i></div>
                 <label for="inputType" class="cellLabel">Videos only</label>
-                <div class="cellValue">
-            		<input type="checkbox" id="inputType" name="Videos only" <?=$videoCheckbox?>
-            		        onchange="window.open(this.value,'_self');" >
-                </div>
+                <input class="cellValue" title="Videos only" type="checkbox" id="inputType" name="type" <?=$videoCheckbox?>>
         		<div class="cellClosingIcon toggle">
         			<label for="inputType"><i></i></label>
         		</div>
             </div>
         </li>
         
-        <li class="cellRow resetFilters">
-            <a class="cellRowContent" href="/">
-                <span class="cellLabel">Apply filters</span>
-            </a>
-        </li>
-    
-<?
-//     if($basepageTwo == 'filtered') {
-?>
-        <li class="cellRow resetFilters">
-            <a class="cellRowContent" href="/">
-                <span class="cellLabel">Reset all filters</span>
-            </a>
-        </li>
-<?
-//     }
-?>
+        <br>
+        
+        <input type="submit" value="Apply filters">
+        <input type="reset" value="Cancel">
+        <a href="/">Reset filters</a>
     </form>
 </ul>
+
 
 
 <ul class="cellRowGroup">
@@ -201,12 +181,5 @@
 
 <script>
 
-/*
-$(document).ready(function() {
-    $('#sel').change(function() {
-	    $('#sel').width( $('#hidnSpan').html($('#sel').find(":selected").text()).width())
-	});
-});
-*/
 
 </script>
