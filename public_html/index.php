1961a9c7317ea9e8d820d38e48eff6fe323437c6
Lex Postma <lex@postma.me>
2017-09-15 13:16:51 +0200

    Remove index debug mode

--- public_html/index.php
+++ public_html/index.php
@@ -27,7 +27,7 @@
 ?>
 
 <!DOCTYPE HTML>
-<html lang="en" class="actionDrawerToggled"> <!-- TODO: actionDrawerToggled -->
+<html lang="en" class=""> <!-- TODO: actionDrawerToggled -->
 <?  include '../includes/head.php'; ?>
 
     <body>
