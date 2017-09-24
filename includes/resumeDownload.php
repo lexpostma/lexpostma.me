cc8c8b765c1871379fd87b8cb960be6f46adc605
Lex Postma <lex@postma.me>
2017-09-24 21:25:56 +0200

    Restyling of download icons with flag

--- includes/resumeDownload.php
+++ includes/resumeDownload.php
@@ -13,7 +13,10 @@
                 download="Lex Postma, web-resume (<? echo date('n/Y')?>).pdf"
                 target="_blank" title="Download a pdf of my resumé in English" 
                 onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf English');">
-            <div class="cellIcon"><? include 'navigationIcons/flagGB.svg'  ?></div>
+            <div class="cellIcon fileNflag">
+                <i class="fa fa-fw fa-file-pdf-o"></i>
+                <? include 'navigationIcons/flagGB.svg'  ?>
+            </div>
             <span class="cellLabel">Resumé in English</span>
             <div class="cellClosingIcon download"><? include 'navigationIcons/download.svg'  ?></div>
         </a>
@@ -23,7 +26,10 @@
                 download="Lex Postma, web-CV (<? echo date('n/Y')?>).pdf"
                 target="_blank" title="Download een pdf van mijn cv in het Nederlands" 
                 onclick="ga('send', 'event', 'Navigation', 'Download', 'Resumé pdf Nederlands');">
-            <div class="cellIcon"><? include 'navigationIcons/flagNL.svg'  ?></div>
+            <div class="cellIcon fileNflag">
+                <i class="fa fa-fw fa-file-pdf-o"></i>
+                <? include 'navigationIcons/flagNL.svg'  ?>
+            </div>
             <span class="cellLabel">CV in het Nederlands</span>
             <div class="cellClosingIcon download"><? include 'navigationIcons/download.svg'  ?></div>
         </a>
