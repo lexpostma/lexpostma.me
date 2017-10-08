86bf99ec8c0b918da6167720d2d0c51ac6a45f38
Lex Postma <lex@postma.me>
2017-10-08 18:09:04 +0200

    Adjust DB connection to allow emoji

--- includes/connection.php
+++ includes/connection.php
@@ -6,5 +6,5 @@
 	define("DATABASE", getenv('LEXPOSTMA_DATABASE'));
 
 	$con = mysqli_connect(HOST,USER,PASSWORD,DATABASE);
-    mysqli_set_charset($con, "utf8");
+    mysqli_set_charset($con, "utf8mb4");
 ?>
