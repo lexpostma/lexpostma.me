1761c49b311cc9272b94ec58b87cf5318034e4e2
Lex Postma <lex@postma.me>
2017-10-10 14:40:37 +0200

    Move Credits and Apple page back under About, disable More about me

--- includes/prehead-about.php
+++ includes/prehead-about.php
@@ -8,27 +8,40 @@
     $seoAuthor      = 'Lex Postma';
     $seoType        = 'website';
 
-
     if(isset($p)) {
-        if($p == 'more'){  // more about me
+        if($p == 'allfunfacts'){  // Show all fun facts
+
+            $funfacts       = true;
+            $includePage    = 'about.php';  
+
+/*
+        } else if($p == 'more'){  // more about me
             $seoTitle       = 'More stuff from Lex';
             $seoDescription = 'More about Lex Postma, including some random stats and his top 5s.'; // his current iOS homescreen, latest backed Kickstarter projects and hardware and software tools he uses.
             $seoKeywords    = 'top5s,stats'; // tools,iOS,homescreen,Kickstarter,software,hardware
             $basepageTwo    = 'more';
             $includePage    = 'aboutMore.php';
+*/
 
-        } else if($p == 'allfunfacts'){  // Show all fun facts
+        } else if($p == 'credits' || $p == 'credit'){  // Credits
 
-            $funfacts       = true;
-            $includePage    = 'about.php';  
+            $seoTitle       = 'Credits for Lex’ website';
+            $seoDescription = $basepageTitle;
+            $seoKeywords    = 'credits';
 
-            
+            $basepageTwo    = 'credits';
+            $includePage    = 'credits.php';  
+
         } else if($p == 'apple'){  // Apple page
+
             $seoTitle       = 'Lex Postma → ';
             $seoDescription = 'Motivation letter from Lex Postma to Apple.';
             $seoKeywords    = 'Apple,design,iOS,culture,passion,software,hardware,prototyping';
+
             $basepageTwo    = 'apple';
             $includePage    = 'apple.php';  
+
+            $disableTitlebar = true;
 
         } else { // Fallback to custom 404 include page
             include '../includes/error-404-include.php'; exit; //Do not do any more work in this script.
