4131ed12cfa7be229960b0b2b3d0f157a6b35dfb
Lex Postma <lex@postma.me>
2017-10-15 12:16:16 +0200

    Make sharing code readable

--- includes/sharing.php
+++ includes/sharing.php
@@ -2,8 +2,38 @@
     $popWindowJS = "javascript:window.open(this.href,'','menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600');return false;";
     
 ?>
-    <a title="Share or comment on Twitter"   class="sharing twitter"   target="_blank" onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Twitter');   <?=$popWindowJS?>" href="https://twitter.com/home?status=<?=$plainTitle?> by @lexpostma • <?=$shareURL?>"><i class="fa fa-twitter"></i>
-</a><a title="Share or comment on Facebook"  class="sharing facebook"  target="_blank" onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Facebook');  <?=$popWindowJS?>" href="https://www.facebook.com/sharer/sharer.php?u=<?=$shareURL?>&t=<?=$plainTitle?>"><i class="fa fa-facebook-square"></i>
-</a><a title="Share or comment on Pinterest" class="sharing pinterest" target="_blank" onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Pinterest'); <?=$popWindowJS?>" href="https://pinterest.com/pin/create/button/?url=<?=$shareURL?>&description=<?=$plainTitle?>"><i class="fa fa-pinterest"></i>
-</a><a title="Share or comment on LinkedIn"  class="sharing linkedin"  target="_blank" onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'LinkedIn');  <?=$popWindowJS?>" href="https://www.linkedin.com/shareArticle?mini=true&url=<?=$shareURL?>&title=<?=$plainTitle?>&source=Lex Postma"><i class="fa fa-linkedin-square"></i>
-</a><a title="Share or comment via email"    class="sharing email"     target="_blank" onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Email');     <?=$popWindowJS?>" href="mailto:hello@lexpostma.me?subject=<?=$plainTitle?>&body=<?=$shareURL?>"><i class="fa fa-envelope"></i></a>
+<a title="Share or comment on Twitter"   
+        class="sharing cellIcon twitter"   
+        target="_blank" 
+        onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Twitter');   <?=$popWindowJS?>"
+        href="https://twitter.com/home?status=<?=$plainTitle?> by @lexpostma • <?=$shareURL?>">
+    <i class="fa fa-twitter"></i>
+</a>
+<a title="Share or comment on Facebook"
+        class="sharing cellIcon facebook"  
+        target="_blank" 
+        onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Facebook');  <?=$popWindowJS?>" 
+        href="https://www.facebook.com/sharer/sharer.php?u=<?=$shareURL?>&t=<?=$plainTitle?>">
+    <i class="fa fa-facebook-square"></i>
+</a>
+<a title="Share or comment on Pinterest" 
+        class="sharing cellIcon pinterest" 
+        target="_blank" 
+        onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Pinterest'); <?=$popWindowJS?>" 
+        href="https://pinterest.com/pin/create/button/?url=<?=$shareURL?>&description=<?=$plainTitle?>">
+    <i class="fa fa-pinterest-p"></i>
+</a>
+<a title="Share or comment on LinkedIn"  
+        class="sharing cellIcon linkedin"  
+        target="_blank" 
+        onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'LinkedIn');  <?=$popWindowJS?>" 
+        href="https://www.linkedin.com/shareArticle?mini=true&url=<?=$shareURL?>&title=<?=$plainTitle?>&source=Lex Postma">
+    <i class="fa fa-linkedin"></i>
+</a>
+<a title="Share or comment via email"    
+        class="sharing cellIcon email"    
+        target="_blank" 
+        onclick="ga('send', 'event', '<?=$shareFrom?>', 'Share', 'Email');     <?=$popWindowJS?>" 
+        href="mailto:hello@lexpostma.me?subject=<?=$plainTitle?>&body=<?=$shareURL?>">
+    <i class="fa fa-envelope"></i>
+</a>
