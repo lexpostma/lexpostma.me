8739e5586e1024bd73e6cf307e1189ffeefc6d55
Lex Postma <lex@postma.me>
2017-12-30 16:28:16 +0100

    Fix database environment variable

--- includes/connection.php
+++ includes/connection.php
@@ -3,7 +3,7 @@
 	define("HOST", getenv('LEXPOSTMA_HOST'));
 	define("USER", getenv('LEXPOSTMA_USER'));
 	define("PASSWORD", getenv('LEXPOSTMA_PASSWORD'));
-	define("DATABASE", getenv('LEXPOSTMA_DATABASE_2017'));
+	define("DATABASE", getenv('LEXPOSTMA_DATABASE'));
 
 	$con = mysqli_connect(HOST,USER,PASSWORD,DATABASE);
     mysqli_set_charset($con, "utf8");
