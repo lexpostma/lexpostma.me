88ebffd9f56210f0d46783dcb97819ca9c2dd610
Lex Postma <lex@postma.me>
2017-12-30 15:18:30 +0100

    Change database

--- includes/connection.php
+++ includes/connection.php
@@ -3,7 +3,7 @@
 	define("HOST", getenv('LEXPOSTMA_HOST'));
 	define("USER", getenv('LEXPOSTMA_USER'));
 	define("PASSWORD", getenv('LEXPOSTMA_PASSWORD'));
-	define("DATABASE", getenv('LEXPOSTMA_DATABASE'));
+	define("DATABASE", getenv('LEXPOSTMA_DATABASE_2017'));
 
 	$con = mysqli_connect(HOST,USER,PASSWORD,DATABASE);
     mysqli_set_charset($con, "utf8");
